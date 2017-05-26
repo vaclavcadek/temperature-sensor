21755aba7c8c7e9e98d5a4b72d7ff8be2f862d20
Václav Čadek <vaclavcadek@gmail.com>
2017-05-26 23:20:06 +0200

    Make it even simpler, using cronjob every minute with same script - no need to schedule within python

--- reader.py
+++ reader.py
@@ -1,17 +1,7 @@
-import sched
-import time
 import serial
 from datetime import datetime
 
-scheduler = sched.scheduler(time.time, time.sleep)
-
-
-def read_signal(sc):
-    with open('temperatures.csv', 'a') as log:
-        s = serial.Serial('/dev/ttyACM0', 9600)
-        t = s.readline().strip()
-        log.write('{timestamp},{temperature}\n'.format(timestamp=datetime.now(), temperature=t))
-    scheduler.enter(1, 1, read_signal, (sc,))
-
-scheduler.enter(1, 1, read_signal, (scheduler,))
-scheduler.run()
+with open('temperatures.csv', 'a') as log:
+    s = serial.Serial('/dev/ttyACM0', 9600)
+    t = s.readline().strip()
+    log.write('{timestamp},{temperature}\n'.format(timestamp=datetime.now(), temperature=t))
