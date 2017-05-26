08e68f74179b33baa666158080443bf30513a74c
Václav Čadek <vaclavcadek@gmail.com>
2017-05-26 22:55:11 +0200

    Changed file location

--- reader.py
+++ reader.py
@@ -7,7 +7,7 @@ scheduler = sched.scheduler(time.time, time.sleep)
 
 
 def read_signal(sc):
-    with open('/home/pi/temperatures.csv', 'a') as log:
+    with open('temperatures.csv', 'a') as log:
         s = serial.Serial('/dev/ttyACM0', 9600)
         t = s.readline().strip()
         log.write('{timestamp},{temperature}\n'.format(timestamp=datetime.now(), temperature=t))
