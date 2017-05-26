b072c6644a91107987f9dc38cdacff5a8c525394
Vaclav Cadek <vaclavcadek@gmail.com>
2017-05-26 20:39:17 +0000

    Minor adjustments

--- reader.py
+++ reader.py
@@ -3,14 +3,15 @@ import time
 import serial
 from datetime import datetime
 
-s = sched.scheduler(time.time, time.sleep)
+scheduler = sched.scheduler(time.time, time.sleep)
 
 
 def read_signal(sc):
     with open('/home/pi/temperatures.csv', 'a') as log:
-        serial = serial.Serial('/dev/ttyACM0', 9600)
-        log.write('{timestamp},{temperature}\n'.format(timestamp=datetime.now(), temperature=23.0))
-    s.enter(1, 1, read_signal, (sc,))
+        s = serial.Serial('/dev/ttyACM0', 9600)
+	t = s,readline().trim()
+        log.write('{timestamp},{temperature}\n'.format(timestamp=datetime.now(), temperature=t))
+    scheduler.enter(1, 1, read_signal, (sc,))
 
-s.enter(1, 1, read_signal, (s,))
-s.run()
+scheduler.enter(1, 1, read_signal, (scheduler,))
+scheduler.run()
