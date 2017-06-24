ea2b32d035407b317c594af9e9b2248951a6b4bd
Václav Čadek <vaclavcadek@gmail.com>
2017-06-24 14:09:59 +0200

    Fail to read from serial port gracefully.

--- reader.py
+++ reader.py
@@ -2,6 +2,9 @@ import serial
 from datetime import datetime
 
 with open('temperatures.csv', 'a') as log:
-    s = serial.Serial('/dev/ttyACM0', 9600)
-    t = s.readline().strip()
-    log.write('{timestamp},{temperature}\n'.format(timestamp=datetime.now(), temperature=t))
+    try:
+        s = serial.Serial('/dev/ttyACM0', 9600)
+        t = s.readline().strip()
+        log.write('{timestamp},{temperature}\n'.format(timestamp=datetime.now(), temperature=t))
+    except serial.SerialException:
+        print('No Serial Port Available!')
