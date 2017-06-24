1356d7b0685719b74362c59d0776ccee46fdc8a0
Václav Čadek <vaclavcadek@gmail.com>
2017-06-24 14:06:25 +0200

    LCD display support

--- temperature.cc
+++ temperature.cc
@@ -1,9 +1,13 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 // named constant for the pin the sensor is connected to
 const int sensorPin = A0;
 
 void setup() {
   // open a serial connection to display values
   Serial.begin(9600);
+  lcd.begin(16, 2);
   // set the LED pins as outputs
   // the for() loop saves some extra coding
   for (int pinNumber = 2; pinNumber < 5; pinNumber++) {
@@ -26,5 +30,9 @@ void loop() {
   // ((volatge - 500mV) times 100)
   float temperature = (voltage - .5) * 100;
   Serial.println(temperature);
-  delay(1000);
+  lcd.setCursor(0, 0);
+  lcd.print("Temperature:");
+  lcd.setCursor(0, 1);
+  lcd.print(String(temperature) + " " + (char) 223 + "C");
+  delay(10000);
 }
