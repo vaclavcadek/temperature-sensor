b5c83110f5e363ea9d96925a3609ff3b52d4e72f
Václav Čadek <vaclavcadek@gmail.com>
2019-01-06 19:33:01 +0000

    Trying to fix problems with reader.

--- reader.py
+++ reader.py
@@ -1,10 +1,13 @@
 import serial
 from datetime import datetime
 
-with open('temperatures.csv', 'a') as log:
-    try:
-        s = serial.Serial('/dev/ttyACM0', 9600)
-        t = s.readline().strip()
-        log.write('{timestamp},{temperature}\n'.format(timestamp=datetime.now(), temperature=t))
-    except serial.SerialException:
-        print('No Serial Port Available!')
+if __name__ == '__main__':
+    with open('temperatures.csv', 'a') as log:
+        try:
+            s = serial.Serial('/dev/ttyACM0')
+            s.flushInput()
+            ser_bytes = s.readline()
+            t = ser_bytes.decode("utf-8")
+            log.write('{timestamp},{temperature}\n'.format(timestamp=datetime.now(), temperature=t))
+        except serial.SerialException:
+            print('No Serial Port Available!')
