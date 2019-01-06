0f117f9a34ccf0f36c399fdea54cf70659ab80a8
Václav Čadek <vaclavcadek@gmail.com>
2019-01-06 19:42:30 +0000

    Fixed problems with reader.

--- reader.py
+++ reader.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
             s.flushInput()
             ser_bytes = s.readline()
             t = ser_bytes.decode('utf-8')
-            log.write('{timestamp},{temperature}\n'.format(timestamp=datetime.now(), temperature=t))
+            log.write('{timestamp},{temperature}'.format(timestamp=datetime.now(), temperature=t))
         except serial.SerialException:
             print('No Serial Port Available!')
