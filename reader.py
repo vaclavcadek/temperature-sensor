2eebf8124ef0ed1c737605801eb4c900eebb4d14
Václav Čadek <vaclavcadek@gmail.com>
2019-01-06 19:33:19 +0000

    Trying to fix problems with reader.

--- reader.py
+++ reader.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
             s = serial.Serial('/dev/ttyACM0')
             s.flushInput()
             ser_bytes = s.readline()
-            t = ser_bytes.decode("utf-8")
+            t = ser_bytes.decode('utf-8')
             log.write('{timestamp},{temperature}\n'.format(timestamp=datetime.now(), temperature=t))
         except serial.SerialException:
             print('No Serial Port Available!')
